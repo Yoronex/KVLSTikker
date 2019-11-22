d8578f33915bf2cf7cb82722af6ad70ff574c08d
Roy (Laptop) <r.t.b.kakkenberg@student.tue.nl>
2019-11-22 20:08:42 +0100

    Moved upgrade to admin panel Added initialization for BigScreen

--- app/socket.py
+++ app/socket.py
@@ -17,25 +17,47 @@ def test_disconnect():
     print('Client disconnected')
 
 
+@socketio.on('init', namespace='/test')
+def init_bigscreen(msg):
+    print(msg)
+    slide_name = msg['slide_name']
+    slide = get_slide_data(slide_name)
+    spotify_data = get_spotify_data()
+    stats_daily, stats_max = get_stats()
+    emit('init', {'slide': {'name': slide_name,
+                            'data': slide},
+                  'spotify': spotify_data,
+                  'stats': {'daily': stats_daily,
+                            'max': stats_max}})
+
+
 @socketio.on('spotify', namespace='/test')
 def update_spotify_request():
+    spotify_data = get_spotify_data()
+    emit('spotify update', spotify_data)
+
+
+def get_spotify_data():
     if spotify.sp is not None:
-        emit('spotify update', {'data': spotify.current_playback(), 'logged in': True})
+        return {'data': spotify.current_playback(), 'logged in': True}
     else:
-        emit('spotify update', {'logged in': False})
+        return {'logged in': False}
+
 
 
 @socketio.on('slide_data', namespace='/test')
 def update_slide_data(msg):
     name = msg["name"]
+    emit('slide_data', {"name": name,
+                        "data": get_slide_data(name)})
 
+
+def get_slide_data(name):
     if name == "DrankTonight":
         data = stats.most_bought_products_by_users_today(datetime.now())
-        emit('slide_data', {"name": name,
-                            "data": {"labels": data[2],
-                                     "values": data[1]}
-                            })
-        return
+        return {'labels': data[2],
+                'values': data[1]}
+
     elif name == "PriceList":
         pnames = []
         prices = []
@@ -43,11 +65,9 @@ def update_slide_data(msg):
         for p in products:
             pnames.append(p.name)
             prices.append("€ {}".format(('%.2f' % p.price).replace('.', ',')))
-        emit('slide_data', {"name": name,
-                            "data": {"names": pnames,
-                                     "prices": prices}
-                            })
-        return
+        return {"names": pnames,
+                "prices": prices}
+
     elif name == "Quote":
         quotes = Quote.query.all()
         q = quotes[randrange(len(quotes))]
@@ -60,25 +80,31 @@ def update_slide_data(msg):
         for u in users:
             unames.append(u.name)
             udebt.append("€ {}".format(('%.2f' % u.balance).replace('.', ',')))
-        emit('slide_data', {'name': name,
-                            "data": {"names": unames,
-                                     "amount": udebt}})
+        return {'names': unames,
+                'amount': udebt}
+
+    return {}
 
 
 def update_stats():
+    daily, maxim = get_stats()
+    socketio.emit('stats', {"daily": daily, "max": maxim}, namespace='/test')
+
+
+def get_stats():
     daily = copy.deepcopy(stats.daily_stats)
-    max = copy.deepcopy(stats.max_stats)
+    maxim = copy.deepcopy(stats.max_stats)
 
     for key, val in daily.items():
         if key != "euros":
             daily[key] = int(val)
-    for key, val in max.items():
+    for key, val in maxim.items():
         if key != "max-stats-euros":
-            max[key] = int(val)
+            maxim[key] = int(val)
     daily["euros"] = "€ {}".format(('%.2f' % daily["euros"]).replace('.', ','))
-    max["max-stats-euros"] = "€ {}".format(('%.2f' % max["max-stats-euros"]).replace('.', ','))
+    maxim["max-stats-euros"] = "€ {}".format(('%.2f' % maxim["max-stats-euros"]).replace('.', ','))
 
-    socketio.emit('stats', {"daily": daily, "max": max}, namespace='/test')
+    return daily, maxim
 
 
 def send_interrupt(message):
