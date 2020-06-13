27b7d2c0b30d256bc2db21aa6ed04c4e2b45022a
Roy (Laptop) <r.t.b.kakkenberg@student.tue.nl>
2020-06-13 20:00:55 +0200

    Moved changelog to separate file

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ class Config(object):
     MAIL_USERNAME = os.getenv('MAIL_USERNAME')
     MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
     MAIL_DEFAULT_SENDER = ("KVLS Tikker", "tikker@kvls.nl")
-    DEBT_MAXIMUM = -30.0
+    DEBT_MAXIMUM = -10.0  # Negative balance is 
     SPOTIPY_CLIENT_ID = os.getenv('SPOTIPY_CLIENT_ID')
     SPOTIPY_CLIENT_SECRET = os.getenv('SPOTIPY_CLIENT_SECRET')
     SPOTIPY_REDIRECT_URI = 'http://127.0.0.1:5000/api/spotify/login'
@@ -35,4 +35,4 @@ class Config(object):
     SPOTIPY_CACHE = '.spotipyoauthcache'
     CALENDAR_URL = 'https://drive.kvls.nl/remote.php/dav/public-calendars/BKWDW9PJT2mmoRa4?export'
     CALENDAR_UPDATE_INTERVAL = 30 * 60
-    PROFIT_PERCENTAGE = 0.8
+    PROFIT_PERCENTAGE = 0.8  # Percentage of profit that goes to the GROUP. The rest goes to the bar!
